a6f49b28f50d4d2f614291e210fc157c58e1192b
Aditya Gupta <ag15035@gmail.com>
2021-02-27 17:23:43 +0530

    Added read_file overload taking ifstream

--- includes/util/file.hpp
+++ includes/util/file.hpp
@@ -6,11 +6,9 @@
 namespace util{
     using path_type = std::string;  // plan to support UTF32 for file names
 
-    std::string read_file(const path_type& path) {
+    std::string read_file(std::ifstream& fin) {
         std::string content;
         std::string tmp;
-
-        std::ifstream fin(path);
 
         if(fin) {
             while (std::getline(fin, tmp) && !fin.eof())
@@ -21,5 +19,10 @@ namespace util{
         }
 
         return content;
+    }
+
+    std::string read_file(const path_type& path) {
+        std::ifstream fin(path);
+        return read_file(fin);
     }
 }
